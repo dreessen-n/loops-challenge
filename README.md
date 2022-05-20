914a034205a160d04a95dd81caa0da7610d810fe
dreessen-n <dreessen.edu@gmail.com>
2022-05-19 18:00:31 -0600

    Initial commit and add first challenge print odds 1-20

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # loops-challenge
+
+Coding Dojo Bootcamp May 2022 - js section - js fundamentals
---
+++ loops-challenge.js
@@ -0,0 +1,9 @@
+// Print odds from 1-20
+
+function oddsOne2Twenty() {
+    for (var i = 1; i <= 20; i+=2) {
+        console.log(i);
+    }
+}
+
+oddsOne2Twenty();
